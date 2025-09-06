2d4707a7fb96336029507176c810e45ca022bc38
Anmol Goel <goelanmol717@gmail.com>
2025-09-06 11:33:23 +0530

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,69 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    },
+    {
+        id:2,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    },
+    {
+        id:3,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    },
+    {
+        id:4,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    },
+    {
+        id:5,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    },
+    {
+        id:6,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    },
+    {
+        id:7,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    },
+    {
+        id:8,
+        name:"Demo",
+        tags:["Demmo1","Demo","Demo"],
+        date:"13 May, 2021",
+        imgSrc:"#",
+        link:"#"
+    }
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,24 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Crypto Market Trends",
+        description:"A Power BI project that visualizes real-time cryptocurrency data, including price movements, market capitalization, and trading volumes",
+        tags:["power bi","excel"],
+        
+        // demo:"#",
+        // github:"#"
+    },
+    {
+        id:2,
+        name:"Credit Card Approvals Analysis & Predictions",
+        description:"A data science project that explores applicant profiles to identify key factors influencing credit card approvals.",
+        tags:["react","sass"],
+        
+         // demo:"#",
+        // github:"#"
+        
+    },
+  
+] 
